a27944fa72af3d6d2b6888a17c7d9d9a408adfd9
inderdeep01 <inderdeepsidhu01@gmail.com>
2022-08-11 03:34:21 +0530

    applied css to images

--- scripts.js
+++ scripts.js
@@ -8,10 +8,43 @@ xhr.onreadystatechange = ()=>{
         const objects = JSON.parse(xhr.responseText)
         var output = ''
         objects.forEach(object => {
-            output+=`<img src="${object.url}">`
+            //output+=`<img src="${object.url}">`
+            document.querySelector('.center').appendChild(createAlbum(object))
         });
-        document.querySelector('.container').innerHTML = output
+        //document.querySelector('.center').innerHTML = output
+        //console.log(output);
     }
 }
 
 xhr.send()
+
+function create(element){
+    const ele = document.createElement(element)
+    return ele
+}
+
+function createAlbum(object){
+    const card = create('div')
+    card.setAttribute('class','card')
+    const anchor = create('a')
+    anchor.setAttribute('href','') //Set the link Here.....................
+    const image = create('div')
+    image.setAttribute('class','image')
+    image.setAttribute('style',`background-image:url(${object.url})`)
+    anchor.appendChild(image)
+
+    const description = create('div')
+    description.setAttribute('class','description')
+    const heading = create('h5')
+    heading.textContent = object.id //albumId
+    const p = create('p')
+    p.textContent = object.title
+    description.appendChild(heading)
+    description.appendChild(p)
+
+    card.appendChild(anchor)
+    card.appendChild(description)
+
+    return card
+}
+
